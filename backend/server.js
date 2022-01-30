cb309915cecbd19f739f11643f72cc8ff615bd47
passionatedeveloper223 <strongwolf223@gmail.com>
2022-01-30 13:47:32 +0500

    fixed dirname

--- backend/server.js
+++ backend/server.js
@@ -46,14 +46,14 @@ app.use("/api/employees", employeeRoutes);
 // const __dirname = path.resolve()
 app.use("/uploads", express.static(path.join(__dirname)));
 
-console.log(path.join(__dirname, "../frontend/build"));
-console.log(path.resolve(__dirname, "../frontend", "build", "index.html"));
+// console.log(path.join(__dirname, "../frontend/build"));
+// console.log(path.resolve(__dirname, "../frontend", "build", "index.html"));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
